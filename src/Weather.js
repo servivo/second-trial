2d91ed6474b14122bc25b3c46c5686e111547833
servivo <91890647+servivo@users.noreply.github.com>
2021-12-06 20:52:09 +0100

    Adjustments of positioning of text

--- src/Weather.js
+++ src/Weather.js
@@ -62,6 +62,8 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <br />
+        <br />
         <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
